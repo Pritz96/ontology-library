1d96dbacd1513f6ed1b9a7f25c9e74d56caa30ea
Pritam Bhudia <pritam1996@hotmail.co.uk>
2026-05-08 15:06:15 +0100

    seperated sosa from ssn and added ontology folder

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ A collection of ontologies/vocabs (in turtle format) all in one place.
 [geo.ttl](https://raw.githubusercontent.com/opengeospatial/ogc-geosparql/refs/heads/master/vocabularies/geo.ttl)
 
 ## Hydra (Core Vocabulary)
-[hydra-core-vocabulary.ttl](https://raw.githubusercontent.com/Pritz96/ontology-library/refs/heads/main/hydra-core-vocabulary.ttl)
+[hydra-core-vocabulary.ttl](https://raw.githubusercontent.com/Pritz96/ontology-library/serialised-ontologies/refs/heads/main/hydra-core-vocabulary.ttl)
 
 ## OWL
 [owl.ttl](https://www.w3.org/2002/07/owl.ttl)
@@ -24,10 +24,13 @@ A collection of ontologies/vocabs (in turtle format) all in one place.
 https://registry.sdmx.org/items/codelist.html
 
 ## SKOS (serialised from RDF/XML into turtle)
-[skos.ttl](https://raw.githubusercontent.com/Pritz96/ontology-library/refs/heads/main/skos.ttl)
+[skos.ttl](https://raw.githubusercontent.com/Pritz96/ontology-library/serialised-ontologies/refs/heads/main/skos.ttl)
 
-## SOSA/SSN Editors Edition (serialised into a single file)
-[sosa-ssn-editors-edition.ttl](https://raw.githubusercontent.com/Pritz96/ontology-library/refs/heads/main/sosa-ssn-editors-edition.ttl)
+## SOSA Editors Edition (serialised into a single file)
+[sosa-editors-edition.ttl](https://raw.githubusercontent.com/Pritz96/ontology-library/serialised-ontologies/refs/heads/main/sosa-editors-edition.ttl)
+
+## SOSA + SSN Editors Edition (serialised into a single file)
+[sosa-ssn-editors-edition.ttl](https://raw.githubusercontent.com/Pritz96/ontology-library/serialised-ontologies/refs/heads/main/sosa-ssn-edition.ttl)
 
 ## XKOS
 [xkos.ttl](https://rdf-vocabulary.ddialliance.org/xkos.ttl)
--- serialise-to-turtle.py
+++ serialise-to-turtle.py
@@ -1,44 +1,52 @@
 from rdflib import Graph
+import os
 
+output_dir = "serialised-ontologies"
+os.makedirs(output_dir, exist_ok=True)
+
+
+## SKOS
 skos_rdf_graph = Graph()
-
 skos_rdf_graph.parse(
     "https://www.w3.org/2009/08/skos-reference/skos.rdf",
     format="xml" 
 )
-
 skos_rdf_graph.serialize(
-    destination="skos.ttl",
+    destination="serialised-ontologies/skos.ttl",
     format="turtle"
 )
 
-sosa_ssn_graph = Graph()
+# SOSA
+sosa_graph = Graph()
+sosa_graph.parse("https://raw.githubusercontent.com/w3c/sdw-sosa-ssn/refs/heads/gh-pages/ssn/rdf/ontology/core/sosa-actuation.ttl", format="turtle")
+sosa_graph.parse("https://raw.githubusercontent.com/w3c/sdw-sosa-ssn/refs/heads/gh-pages/ssn/rdf/ontology/core/sosa-common.ttl", format="turtle")
+sosa_graph.parse("https://raw.githubusercontent.com/w3c/sdw-sosa-ssn/refs/heads/gh-pages/ssn/rdf/ontology/core/sosa-deprecated.ttl", format="turtle")
+sosa_graph.parse("https://raw.githubusercontent.com/w3c/sdw-sosa-ssn/refs/heads/gh-pages/ssn/rdf/ontology/core/sosa-observation.ttl", format="turtle")
+sosa_graph.parse("https://raw.githubusercontent.com/w3c/sdw-sosa-ssn/refs/heads/gh-pages/ssn/rdf/ontology/core/sosa-sampling.ttl", format="turtle")
+sosa_graph.parse("https://raw.githubusercontent.com/w3c/sdw-sosa-ssn/refs/heads/gh-pages/ssn/rdf/ontology/core/sosa.ttl", format="turtle")
+sosa_graph.serialize(
+    destination="sosa-editors-edition.ttl",
+    format="turtle"
+)
 
-# Load SOSA
-sosa_ssn_graph.parse("https://raw.githubusercontent.com/w3c/sdw-sosa-ssn/refs/heads/gh-pages/ssn/rdf/ontology/core/sosa-actuation.ttl", format="turtle")
-sosa_ssn_graph.parse("https://raw.githubusercontent.com/w3c/sdw-sosa-ssn/refs/heads/gh-pages/ssn/rdf/ontology/core/sosa-common.ttl", format="turtle")
-sosa_ssn_graph.parse("https://raw.githubusercontent.com/w3c/sdw-sosa-ssn/refs/heads/gh-pages/ssn/rdf/ontology/core/sosa-deprecated.ttl", format="turtle")
-sosa_ssn_graph.parse("https://raw.githubusercontent.com/w3c/sdw-sosa-ssn/refs/heads/gh-pages/ssn/rdf/ontology/core/sosa-observation.ttl", format="turtle")
-sosa_ssn_graph.parse("https://raw.githubusercontent.com/w3c/sdw-sosa-ssn/refs/heads/gh-pages/ssn/rdf/ontology/core/sosa-sampling.ttl", format="turtle")
-sosa_ssn_graph.parse("https://raw.githubusercontent.com/w3c/sdw-sosa-ssn/refs/heads/gh-pages/ssn/rdf/ontology/core/sosa.ttl", format="turtle")
-
-# Load SSN
-sosa_ssn_graph.parse("https://raw.githubusercontent.com/w3c/sdw-sosa-ssn/refs/heads/gh-pages/ssn/rdf/ontology/core/ssn-actuation.ttl", format="turtle")
-sosa_ssn_graph.parse("https://raw.githubusercontent.com/w3c/sdw-sosa-ssn/refs/heads/gh-pages/ssn/rdf/ontology/core/ssn-common.ttl",format="turtle")
-sosa_ssn_graph.parse("https://raw.githubusercontent.com/w3c/sdw-sosa-ssn/refs/heads/gh-pages/ssn/rdf/ontology/core/ssn-deprecated.ttl",format="turtle")
-sosa_ssn_graph.parse("https://raw.githubusercontent.com/w3c/sdw-sosa-ssn/refs/heads/gh-pages/ssn/rdf/ontology/core/ssn-observation.ttl",format="turtle")
-sosa_ssn_graph.parse("https://raw.githubusercontent.com/w3c/sdw-sosa-ssn/refs/heads/gh-pages/ssn/rdf/ontology/core/ssn-sampling.ttl",format="turtle")
-sosa_ssn_graph.parse("https://raw.githubusercontent.com/w3c/sdw-sosa-ssn/refs/heads/gh-pages/ssn/rdf/ontology/core/ssn.ttl",format="turtle")
-
+# SSN
+ssn_graph = Graph()
+ssn_graph.parse("https://raw.githubusercontent.com/w3c/sdw-sosa-ssn/refs/heads/gh-pages/ssn/rdf/ontology/core/ssn-actuation.ttl", format="turtle")
+ssn_graph.parse("https://raw.githubusercontent.com/w3c/sdw-sosa-ssn/refs/heads/gh-pages/ssn/rdf/ontology/core/ssn-common.ttl",format="turtle")
+ssn_graph.parse("https://raw.githubusercontent.com/w3c/sdw-sosa-ssn/refs/heads/gh-pages/ssn/rdf/ontology/core/ssn-deprecated.ttl",format="turtle")
+ssn_graph.parse("https://raw.githubusercontent.com/w3c/sdw-sosa-ssn/refs/heads/gh-pages/ssn/rdf/ontology/core/ssn-observation.ttl",format="turtle")
+ssn_graph.parse("https://raw.githubusercontent.com/w3c/sdw-sosa-ssn/refs/heads/gh-pages/ssn/rdf/ontology/core/ssn-sampling.ttl",format="turtle")
+ssn_graph.parse("https://raw.githubusercontent.com/w3c/sdw-sosa-ssn/refs/heads/gh-pages/ssn/rdf/ontology/core/ssn.ttl",format="turtle")
+sosa_ssn_graph = sosa_graph + ssn_graph
 sosa_ssn_graph.serialize(
-    destination="sosa-ssn-editors-edition.ttl",
+    destination="serialised-ontologies/sosa-ssn-editors-edition.ttl",
     format="turtle"
 )
 
-# Load and serialise hydra json-ld into turtle as the turtle link is broken
+# Hydra (the official ttl link is broken so serialising the json-ld into ttl)
 hydra = Graph()
 hydra.parse("https://www.hydra-cg.com/spec/latest/core/core.jsonld", format="json-ld")
 hydra.serialize(
-    destination="hydra.ttl",
+    destination="serialised-ontologies/hydra-core-vocabulary.ttl",
     format="turtle"
 )
